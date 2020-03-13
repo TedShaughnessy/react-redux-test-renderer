5b5973a21c569be9395a6f8fd6785243355c217f
ted <ted.shaughnessy@iress.com>
2020-03-13 17:20:56 +0000

    fixed useContextValue and added tests

--- src/ComponentMethods.tsx
+++ src/ComponentMethods.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { _component, IWrapper, IComponent } from './types';
+import { _component } from './types';
 import { TestRendererBase } from './TestRendererBase';
 
 export class ComponentMethods {
@@ -28,10 +28,30 @@ export class ComponentMethods {
     useWrapperProps = (id: number, props: object): void => {
         this.enableTemporaryWrappers();
 
-        this.modifyWrapperPropsArray(this.trb.temporaryWrappers, id, props);
+        if (!this.isValidIndex(id)) {
+            throw Error(
+                `useWrapperProps 'id' is not valid, id is: ${id} ` +
+                    `the wrapper array has a length of ${this.trb.temporaryWrappers.length}`
+            );
+        }
+
+        const wrapperArray = this.trb.temporaryWrappers;
+        wrapperArray[id] = { wrapper: { ...wrapperArray[id].wrapper, props } };
     };
 
-    useContextValue = (id: number, props: object): void => this.useWrapperProps(id, props);
+    useContextValue = (id: number, value: object): void => {
+        this.enableTemporaryWrappers();
+
+        if (!this.isValidIndex(id)) {
+            throw Error(
+                `useContextValue 'id' is not valid, id is: ${id} ` +
+                    `the wrapper array has a length of ${this.trb.temporaryWrappers.length}`
+            );
+        }
+
+        const wrapperArray = this.trb.temporaryWrappers;
+        wrapperArray[id] = { wrapper: { ...wrapperArray[id].wrapper, value } };
+    };
 
     private enableTemporaryWrappers = (): void => {
         if (!this.trb.useTemporaryWrappers) {
@@ -40,14 +60,5 @@ export class ComponentMethods {
         }
     };
 
-    private modifyWrapperPropsArray = (array: IWrapper[], index: number, props: object): _component => {
-        const wrapper = array[index].wrapper as IComponent;
-
-        if (wrapper.component === undefined) {
-            throw new Error('not a component');
-        }
-
-        // eslint-disable-next-line no-param-reassign
-        array[index] = { wrapper: { component: wrapper.component, props } };
-    };
+    private isValidIndex = (id: number): boolean => id >= 0 && id < this.trb.temporaryWrappers.length;
 }
--- src/tests/contextConsumer.test.tsx
+++ src/tests/contextConsumer.test.tsx
@@ -4,7 +4,10 @@ import { TestContextConsumerComponent } from './testComponents/TestContextConsum
 import { defaultContextState, TestContext } from './testComponents/TestContextProviderWrapper';
 
 const testComponentWithContext = new TestRenderer(TestContextConsumerComponent, defaultProps, defaultState);
-testComponentWithContext.addContextProvider(TestContext, { ...defaultContextState, contextProp: 1 });
+const contextValue = testComponentWithContext.addContextProvider(TestContext, {
+    ...defaultContextState,
+    contextProp: 1,
+});
 
 const selector = 'body > div > h3';
 
@@ -13,6 +16,13 @@ afterEach(cleanup);
 describe('adding context provider wrapper works: ', () => {
     it('context consumer component', () => {
         const result = testComponentWithContext.render();
+
+        expect(getNullSafeTextContent(result, selector)).toEqual('1');
+    });
+    it('context consumer component', () => {
+        const result = testComponentWithContext
+            .useContextValue(contextValue, { ...defaultContextState, contextProp: 2 })
+            .render();
 
         expect(getNullSafeTextContent(result, selector)).toEqual('1');
     });
